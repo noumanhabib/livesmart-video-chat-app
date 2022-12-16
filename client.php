b059e2a4e658dffe97b98d75d412851655b83a5f
Nouman Habib <noumanhabib521@gmail.com>
2022-12-16 18:38:23 +0500

    New index

--- client.php
+++ client.php
@@ -8,24 +8,28 @@ if (!isset($_GET["id"])) {
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>    
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LiveSmart Client</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Favicon -->
-        <link rel="shortcut icon" href="img/favicon.ico">
+    <title>LiveSmart Client</title>
 
-
-
-    </head>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon.ico">
 
 
-    <body>
-        <div id="nd-widget-container" class="nd-widget-container"></div>
-        <script id="newdev-embed-script" data-message="Start Video Chat" data-agent_id="<?php echo $agentId; ?>" data-source_path="YOUR_DOMAIN/" src="YOUR_DOMAIN/js/widget.js" data-button-css="button_gray.css" data-avatar="../img/avatar.png" data-names="John Doe" async></script>
 
-    </body>
+</head>
+
+
+<body>
+    <div id="nd-widget-container" class="nd-widget-container"></div>
+    <script id="newdev-embed-script" data-message="Start Video Chat" data-agent_id="<?php echo $agentId; ?>"
+        data-source_path="/" src="/js/widget.js" data-button-css="button_gray.css" data-avatar="../img/avatar.png"
+        data-names="John Doe" async></script>
+
+</body>
+
 </html>
